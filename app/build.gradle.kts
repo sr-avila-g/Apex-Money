6eac3f7f19c73e2ee7a6ea9fde8b21c3238244b4
sravilagarcia360 <sr.avila.garcia.360@gmail.com>
2026-09-13 14:49:30 -0400

    Mejora Funcionalidades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sravila.apexmoney"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
